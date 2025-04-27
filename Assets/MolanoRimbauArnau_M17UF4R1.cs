f7ffbef47697d34bc41fd0bcb65f66cf18fa7fa0
MolaniTTo <arnau.molano.7e7@itb.cat>
2025-04-27 21:16:15 +0200

    implementat moviment del personatge, 2 camares de primera i tercera persona, animacions fetes, implementat mascara de animacions, apuntar, IK amb les mans

--- Assets/MolanoRimbauArnau_M17UF4R1.cs
+++ Assets/MolanoRimbauArnau_M17UF4R1.cs
@@ -80,6 +80,15 @@ public partial class @MolanoRimbauArnau_M17UF4R1: IInputActionCollection2, IDisp
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""Aim"",
+                    ""type"": ""Button"",
+                    ""id"": ""1b707dc3-29a7-4edd-8971-38fc244acc53"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
                 }
             ],
             ""bindings"": [
@@ -306,7 +315,7 @@ public partial class @MolanoRimbauArnau_M17UF4R1: IInputActionCollection2, IDisp
                 {
                     ""name"": """",
                     ""id"": ""ce4e7581-3e7e-4c57-9597-44d45c606545"",
-                    ""path"": ""<Keyboard>/f"",
+                    ""path"": ""<Keyboard>/space"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
@@ -355,6 +364,17 @@ public partial class @MolanoRimbauArnau_M17UF4R1: IInputActionCollection2, IDisp
                     ""processors"": """",
                     ""groups"": """",
                     ""action"": ""Crouch"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""67d17ecf-197d-4d7b-8c2c-ba3b5c361f21"",
+                    ""path"": ""<Mouse>/rightButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Aim"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
@@ -948,6 +968,7 @@ public partial class @MolanoRimbauArnau_M17UF4R1: IInputActionCollection2, IDisp
         m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
         m_Player_Run = m_Player.FindAction("Run", throwIfNotFound: true);
         m_Player_Crouch = m_Player.FindAction("Crouch", throwIfNotFound: true);
+        m_Player_Aim = m_Player.FindAction("Aim", throwIfNotFound: true);
         // UI
         m_UI = asset.FindActionMap("UI", throwIfNotFound: true);
         m_UI_Navigate = m_UI.FindAction("Navigate", throwIfNotFound: true);
@@ -1027,6 +1048,7 @@ public partial class @MolanoRimbauArnau_M17UF4R1: IInputActionCollection2, IDisp
     private readonly InputAction m_Player_Jump;
     private readonly InputAction m_Player_Run;
     private readonly InputAction m_Player_Crouch;
+    private readonly InputAction m_Player_Aim;
     public struct PlayerActions
     {
         private @MolanoRimbauArnau_M17UF4R1 m_Wrapper;
@@ -1037,6 +1059,7 @@ public partial class @MolanoRimbauArnau_M17UF4R1: IInputActionCollection2, IDisp
         public InputAction @Jump => m_Wrapper.m_Player_Jump;
         public InputAction @Run => m_Wrapper.m_Player_Run;
         public InputAction @Crouch => m_Wrapper.m_Player_Crouch;
+        public InputAction @Aim => m_Wrapper.m_Player_Aim;
         public InputActionMap Get() { return m_Wrapper.m_Player; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -1064,6 +1087,9 @@ public partial class @MolanoRimbauArnau_M17UF4R1: IInputActionCollection2, IDisp
             @Crouch.started += instance.OnCrouch;
             @Crouch.performed += instance.OnCrouch;
             @Crouch.canceled += instance.OnCrouch;
+            @Aim.started += instance.OnAim;
+            @Aim.performed += instance.OnAim;
+            @Aim.canceled += instance.OnAim;
         }
 
         private void UnregisterCallbacks(IPlayerActions instance)
@@ -1086,6 +1112,9 @@ public partial class @MolanoRimbauArnau_M17UF4R1: IInputActionCollection2, IDisp
             @Crouch.started -= instance.OnCrouch;
             @Crouch.performed -= instance.OnCrouch;
             @Crouch.canceled -= instance.OnCrouch;
+            @Aim.started -= instance.OnAim;
+            @Aim.performed -= instance.OnAim;
+            @Aim.canceled -= instance.OnAim;
         }
 
         public void RemoveCallbacks(IPlayerActions instance)
@@ -1274,6 +1303,7 @@ public partial class @MolanoRimbauArnau_M17UF4R1: IInputActionCollection2, IDisp
         void OnJump(InputAction.CallbackContext context);
         void OnRun(InputAction.CallbackContext context);
         void OnCrouch(InputAction.CallbackContext context);
+        void OnAim(InputAction.CallbackContext context);
     }
     public interface IUIActions
     {
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
@@ -15,22 +16,35 @@ public class PlayerController : MonoBehaviour, @MolanoRimbauArnau_M17UF4R1.IPlay
     [SerializeField] private float walkSpeed = 1.0f;
     [SerializeField] private float runSpeed = 4.0f;
     [SerializeField] private float crouchSpeed = 4.0f;
-
-
     [SerializeField] private float jumpForce = 5.0f;
+    [SerializeField] private float rotationSpeed = 100.0f; //velocitat de rotacio del jugador (per la camera)
+    private float aimBlendSpeed = 5.0f; //velocitat d'animacio de la transicio entre el layer 0 i el layer 1 (aim)
+
+    [SerializeField] private int upperBodyLayerIndex = 1; // Index del layer d'aim (0 es el base layer, 1 es el d'aim)
 
     private Vector2 inputVector = Vector2.zero;
     private bool isGrounded = false;
     private bool isRunning = false;
     private bool isCrouched = false;
+    private bool isAiming = false;
+    private bool isShooting = false; // Variable para controlar el disparo
+
+
+    [SerializeField] private CinemachineVirtualCamera thirdPersonCam;
+    [SerializeField] private CinemachineVirtualCamera firstPersonCam;
+
 
     void Awake()
     {
+
         rb = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
         playerInput = GetComponent<PlayerInput>();
         controls = new @MolanoRimbauArnau_M17UF4R1();
         controls.Player.SetCallbacks(this);
+
+        //bloquear el cursor
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void OnEnable()
@@ -46,7 +60,9 @@ public class PlayerController : MonoBehaviour, @MolanoRimbauArnau_M17UF4R1.IPlay
     void Update()
     {
         CheckGrounded();
-        UpdateAnimation();
+        UpdateBaseLayerAnimation();
+        UpdateJumpHeight();
+        UpdateAimLayerAnimation();
     }
 
     void FixedUpdate()
@@ -58,17 +74,18 @@ public class PlayerController : MonoBehaviour, @MolanoRimbauArnau_M17UF4R1.IPlay
     {
         Vector3 movement = new Vector3(inputVector.x, 0, inputVector.y);
 
+        movement = transform.TransformDirection(movement); // Transformar la dirección de movimiento a espacio local
+
         // Ajustar la velocidad en función de si está agachado o no
         float currentSpeed = isCrouched ? crouchSpeed : (isRunning ? runSpeed : walkSpeed);
 
         // Usamos la velocidad directamente para evitar problemas de aplicación de fuerza
         Vector3 moveVelocity = movement * currentSpeed;
 
-        // Solo modificamos la velocidad en el plano horizontal (X y Z), manteniendo la Y intacta
         rb.velocity = new Vector3(moveVelocity.x, rb.velocity.y, moveVelocity.z);
 
         // Debugging: Verifica las velocidades en cada frame
-        Debug.Log($"Current Speed: {currentSpeed}, Velocity: {moveVelocity}, Input Vector: {inputVector}");
+        //Debug.Log($"Current Speed: {currentSpeed}, Velocity: {moveVelocity}, Input Vector: {inputVector}");
     }
 
 
@@ -85,6 +102,11 @@ public class PlayerController : MonoBehaviour, @MolanoRimbauArnau_M17UF4R1.IPlay
         isGrounded = Physics.SphereCast(spherePosition, 0.3f, Vector3.down, out _, rayLength, groundLayer);
         Debug.DrawRay(spherePosition, Vector3.down * rayLength, isGrounded ? Color.green : Color.red);
 
+        if (isGrounded && animator != null)
+        {
+            animator.SetFloat("JumpHeight", 0f);
+        }
+        
         if (animator != null)
         {
             animator.SetBool("isGrounded", isGrounded);
@@ -96,12 +118,39 @@ public class PlayerController : MonoBehaviour, @MolanoRimbauArnau_M17UF4R1.IPlay
         if (isGrounded)
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            if (animator != null)
-                animator.SetTrigger("Jump");
         }
     }
 
-    private void UpdateAnimation()
+    private void UpdateJumpHeight()
+    {
+        float jumpHeight = rb.velocity.y;
+
+        if (jumpHeight > 0)
+        {
+            jumpHeight = Mathf.Clamp01(jumpHeight / jumpForce); //que no sigui mes gran que 1
+        }
+
+        if (!isGrounded && jumpHeight < 0f) //si esta caient
+        {
+            jumpHeight = Mathf.Clamp01(-jumpHeight / jumpForce); //normalitzem la altura del salt en el rang [0, 1] (que no sigui -1 perque faria la animacio de jumpdown)
+        }
+        else if (isGrounded)
+        {
+            //quan toca el terra, jumpheight es -1 per l'animacio de jumpdown
+            jumpHeight = -1f;
+        }
+        if (!isGrounded && jumpHeight < 0.5f && jumpHeight > 0f)
+        {
+            jumpHeight = Mathf.Lerp(jumpHeight, 0f, Time.deltaTime * 5); // Suaviza la caída
+        }
+
+        if (animator != null)
+        {
+            animator.SetFloat("JumpHeight", jumpHeight, 0.1f, Time.deltaTime);
+        }
+    }
+
+    private void UpdateBaseLayerAnimation() //actualitza l'animacio del base layer
     {
         float targetVelocity = 0f;
 
@@ -119,13 +168,25 @@ public class PlayerController : MonoBehaviour, @MolanoRimbauArnau_M17UF4R1.IPlay
         }
     }
 
+    private void UpdateAimLayerAnimation()
+    {
+        if(animator != null)
+        {
+            float aimWeight = isAiming ? 1f : 0f; //es el weight del layer d'aim
+            float currentWeight = animator.GetLayerWeight(upperBodyLayerIndex); //layer 1 es el d'aim
+            float newWeight = Mathf.Lerp(currentWeight, aimWeight, Time.deltaTime * aimBlendSpeed); //fa la transicio entre el layer 0 i el layer 1
+            animator.SetLayerWeight(upperBodyLayerIndex, newWeight); //actualitza el weight del layer d'aim
+        }
+    }
+
+
 
 
     // INPUTS
     public void OnMove(InputAction.CallbackContext context)
     {
         inputVector = context.ReadValue<Vector2>();
-        Debug.Log($"Input Vector: {inputVector}");
+        //Debug.Log($"Input Vector: {inputVector}");
     }
 
     public void OnJump(InputAction.CallbackContext context)
@@ -138,12 +199,39 @@ public class PlayerController : MonoBehaviour, @MolanoRimbauArnau_M17UF4R1.IPlay
 
     public void OnLook(InputAction.CallbackContext context)
     {
-        // Implementación de rotación de cámara (si aplica)
+        Vector2 lookInput = context.ReadValue<Vector2>();
+        if (lookInput != Vector2.zero)
+        {
+            //rotar el jugador en 2 ejes
+            float rotationX = lookInput.x * rotationSpeed * Time.deltaTime;
+            float rotationY = lookInput.y * rotationSpeed * Time.deltaTime;
+
+            transform.Rotate(rotationX,rotationY , 0); //rotar el jugador en el eix Y
+            
+        }
+        
     }
 
     public void OnFire(InputAction.CallbackContext context)
     {
-        // Acción de ataque, si aplica
+        
+        if (context.performed)
+        {
+            isShooting = true; //activem el dispar
+            if(animator != null)
+            {
+                animator.SetBool("Fire", isShooting);
+            }
+        }
+        else if (context.canceled)
+        {
+            isShooting = false; //desactivem el dispar
+            if (animator != null)
+            {
+                animator.SetBool("Fire", isShooting);
+            }
+
+        }
     }
 
     public void OnRun(InputAction.CallbackContext context)
@@ -173,6 +261,20 @@ public class PlayerController : MonoBehaviour, @MolanoRimbauArnau_M17UF4R1.IPlay
         {
             Debug.Log("Crouched: " + isCrouched);
             animator.SetBool("Crouched", isCrouched);
+        }
+    }
+
+    public void OnAim(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            isAiming = true;
+            firstPersonCam.Priority = 20; //activa la camara de 1a persona
+        }
+        else if (context.canceled)
+        {
+            isAiming = false;
+            firstPersonCam.Priority = 5; //activa la camara de 1a persona
         }
     }
 }
